099d8d80b181584ce9260560bfa48ce5b891ab38
EDIS26 <es.futures26@gmail.com>
2018-11-18 17:46:47 +0700

    hs

--- src/App.js
+++ src/App.js
@@ -1,45 +1,34 @@
 import React, { Component } from 'react';
 
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-
-      developer: [
-        {
-          id: 1,
-          nama: 'Edi Santoso',
-        },
-
-        {
-          id: 2,
-          nama: 'Suranta Petrus',
-        },
-
-        {
-          id: 3,
-          nama: 'Abrar Chandra',
-        }
-      ]
-
-    }
+    this.state = { ITDivision: [] }
   }
 
+  componentDidMount() {
+    fetch("https://raw.githubusercontent.com/EDIS26/reak/master/homestays.json")
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({ homestays: data })
+      }
+      )
+  }
   render() {
     return (
       <div>
         {
-          this.state.developer.map((dinamis, Key) =>
+          this.state.ITDivision.map((dinamis, key) =>
             <div>
-              <h3>{dinamis.id} {dinamis.nama}</h3>
-
+              <h3>{dinamis.npk} {dinamis.nama} {dinamis.alamat} rb</h3>
             </div>
-
           )
         }
       </div>
     );
   }
+
 }
 
 export default App;
